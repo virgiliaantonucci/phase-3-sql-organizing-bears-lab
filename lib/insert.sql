6a1cca5552e35cbd53d8225e6ac3f642a8338442
Virgilia Antonucci <virgilia.antonucci@gmail.com>
2022-07-05 21:57:36 -0400

    commit tree 847c32284b098b89802ab97becdf9831005ee38e

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (1, 'Mr. Chocolate', 2, 'M', 'brown', 'sweet', 1),
+    (2, 'Rowdy', 2, 'M', 'brown', 'sweet', 1),
+    (3, 'Tabitha', 2, 'F', 'brown', 'sweet', 1),
+    (4, 'Sergeant Brown', 2, 'F', 'brown', 'sweet', 1),
+    (5, 'Melissa', 2, 'F', 'brown', 'sweet', 1),
+    (6, 'Grinch', 2, 'M', 'brown', 'sweet', 1),
+    (7, 'Wendy', 2, 'F', 'brown', 'sweet', 1),
+    (8, NULL, 2, 'M', 'brown', 'sweet', 1);
